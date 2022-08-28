e88c5e5e87bf996c14a0562a4752b5b5d660b81a
lee sock won <lsw4uto@gmail.com>
2022-08-28 19:39:13 +0900

    큰 수정사항은 없음

--- axiosForFront.js
+++ axiosForFront.js
@@ -1,9 +1,26 @@
 const axios = require("axios");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
+const {logger} = require("./logs/config/winston")
 const key = "secret"
 const IP = "192.168.219.101"
 const PORT = "8000"
+
+const bodyLogin = {
+    username : "hello@gmail.com",
+    password : "3039398"
+}
+
+const bodySignup = {
+    // name : "leesockwon3",
+    username : "", 
+    password : "abcd0000", 
+    // birth : "1990-01-10", 
+    // contact : "119-003-555-1234", 
+    // address : "seoul gangnam"
+}
+
+/* ------------------------------------------------------- */
 
 const instance = axios.create({
     baseURL : `http://${IP}:${PORT}/`,
@@ -16,28 +33,20 @@ const healthCheck = async ()=>{
 
 const signup = async ()=>{
     try{
-        return await instance.post("users/signup", {
-        name : "leesockwon3",
-        username : "foobar6@gmail.com", 
-        password : "abcd0000", 
-        birth : "1990-01-10", 
-        contact : "119-003-555-1234", 
-        address : "seoul gangnam"
-    })
+        await instance.post("users/signup", bodySignup)
+        logger.lnfo(`POST / signup: ${bodySignup !== undefined}`)
+
     }catch(err){
+        logger.error(`POST / signup: ${err.statusCode}`)
         console.log(err);
     }
 }
 
 const login = async ()=>{
-    try{
-        return await instance.post("users/login", {
-            username : "foobar1@gmail.com",
-            password : "abcd1111"
-        })
-    }catch(err){
-        console.log(err)
-    }
+    return await instance.post("users/login", {
+        username : "hello48@gmail.com",
+        password : "abcd0000"
+    })
 }
 
 const init = async ()=>{
@@ -47,14 +56,19 @@ const init = async ()=>{
 }
 
 const userLogin = async ()=>{
-    const llog = await login();
-    console.log(llog.data)
-    const token = llog.data.token;
-    const payLoad = jwt.verify(token, key);
-    const {name, username} = payLoad;
-    console.log(name, username)
-    const data = `username : ${username}, token : ${token}\n`
-    fs.appendFileSync('tokenList.txt',data);
+    try{
+        const llog = await login();
+        const token = llog.data.token;
+        const payLoad = jwt.verify(token, key);
+        const {name, username} = payLoad;
+        const data = `IP & PORT : ${IP}:${PORT}, username : ${username}, token : ${token}`
+        return logger.info(`POST / login: ${token !== undefined}, ${data}`)
+        
+    }catch(err){
+        logger.error(`POST / login: 패스워드 입력값=>${err.data}, 헤더=>${err.headers}`)
+        throw err
+    }
+    // fs.appendFileSync('./tokenList.txt',data);
 }
 
 const doIt =async ()=>{
